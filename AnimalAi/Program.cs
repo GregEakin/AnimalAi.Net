a5022798a6fdeb868a06426114cd763cf082f805
Greg Eakin <gregory.eakin@gmail.com>
2019-05-18 20:50:58 -0700

    Add project files.

---
+++ AnimalAi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalAi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
