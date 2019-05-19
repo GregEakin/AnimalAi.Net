c493123aa3749fdfac08a63cc2720e08ac6610d9
Greg Eakin <gregory.eakin@gmail.com>
2019-05-19 07:31:49 -0700

    Code cleanup in InMemoryDatabase

--- AnimalAiUnitTests/InMemoryDatabase.cs
+++ AnimalAiUnitTests/InMemoryDatabase.cs
@@ -3,18 +3,11 @@ using NHibernate;
 using NHibernate.Cfg;
 using NHibernate.Dialect;
 using NHibernate.Driver;
-using NHibernate.Tool.hbm2ddl;
-using System;
-using System.Reflection;
 
 namespace AnimalAiUnitTests
 {
-    public sealed class InMemoryDatabase : IDisposable
+    public static class InMemoryDatabase
     {
-        private static readonly Lazy<Configuration> Configuration = new Lazy<Configuration>(SetupConfiguration);
-        private static readonly Lazy<ISessionFactory> SessionFactory = new Lazy<ISessionFactory>(() => Configuration.Value.BuildSessionFactory());
-        private readonly ISession _session;
-
         public static Configuration SetupConfiguration()
         {
             var cfg = new Configuration();
@@ -33,18 +26,6 @@ namespace AnimalAiUnitTests
             cfg.AddAssembly(typeof(Question).Assembly);
 
             return cfg;
-        }
-
-        public InMemoryDatabase()
-        {
-            _session = SessionFactory.Value.OpenSession();
-            new SchemaExport(Configuration.Value).SetOutputFile("schema.sql").Execute(true, true, false);
-            // new SchemaExport(Configuration.Value).Execute(true, true, false, true, _session.Connection, Console.Out);
-        }
-
-        public void Dispose()
-        {
-            _session.Dispose();
         }
     }
 }
