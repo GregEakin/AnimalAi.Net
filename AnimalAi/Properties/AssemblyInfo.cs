56e78a68b2aac86b3ca04dd740eaed0a7a5fd155
Greg Eakin <gregory.eakin@gmail.com>
2019-05-19 14:56:49 -0700

    Update Assemly Info

--- AnimalAi/Properties/AssemblyInfo.cs
+++ AnimalAi/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AnimalAi")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("Animal AI")]
+[assembly: AssemblyDescription("Self-Learning Animal Identifer")]
+[assembly: AssemblyConfiguration("Console Application")]
+[assembly: AssemblyCompany("Greg Eakin")]
 [assembly: AssemblyProduct("AnimalAi")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Greg Eakin 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
